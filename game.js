12565a4f69ef493bae5f024b0a19c39e9a2290d2
Kristupas <108592864+removeris@users.noreply.github.com>
2024-09-06 22:26:00 +0300

    Add game logic, finish game output

--- game.js
+++ game.js
@@ -7,8 +7,7 @@ function getComputerChoice(){
         case 1:
             return "paper";
         case 2:
-            return "scissors"; 
-            
+            return "scissors";      
     }
 }
 
@@ -16,6 +15,8 @@ function getHumanChoice(){
     let choice;
     do{
         choice = prompt("Choose rock, paper or scissors");
+        choice = choice.toLowerCase();
+
         if (choice === 'rock' || choice === 'paper' || choice === 'scissors'){
             break;
         }
@@ -26,3 +27,41 @@ function getHumanChoice(){
     
     return choice;
 }
+
+function playRound(humanChoice, computerChoice){
+    if((humanChoice == 'rock' && computerChoice == 'scissors') ||
+       (humanChoice == 'scissors' && computerChoice == 'paper') ||
+       (humanChoice == 'paper' && computerChoice == 'rock') ){
+        
+        console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+        humanScore++;
+    }
+    else if(humanChoice == computerChoice){
+        console.log(`It's a draw. You both picked ${humanChoice}.`);
+    }
+    else{
+        console.log(`You lost.. ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }
+}
+
+function playGame(){
+    for(let i = 0; i < 5; i++){
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+        console.log(`The Score is [${humanScore}:${computerScore}]`);
+    }
+}
+
+let humanScore = 0, computerScore = 0;
+
+playGame();
+
+
+
+
+
+
+
+
